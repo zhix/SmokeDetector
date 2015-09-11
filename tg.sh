7c7d17c88ba8dce0e7b591910193b5f726200c55
zhix <zhix.chong@gmail.com>
2015-09-11 11:56:37 +0800

    11Sep_3

--- tg.sh
+++ tg.sh
@@ -3,9 +3,14 @@
   to=$1
   msg=$2
   tgpath=/home/pi/tg
+  result = 0 
 
-  cd ${tgpath}
-  (sleep 3;echo "get_self";sleep 2 ; echo "msg $to $msg"; echo "safe_quit") | ${tgpath}/bin/telegram-cli -k tg-server.pub -W
+#  sudo python connect_to_arduino.py
+#  while True:
+#  if  result > 100,
+	 cd ${tgpath}
+	(sleep 3;echo "get_self";sleep 2 ; echo "msg $to $msg"; echo "safe_quit") | ${tgpath}/bin/telegram-cli -k tg-server.pub -W
+
 
 
 ##  ${tgpath}/bin/telegram-cli -k tg-server.pub -W | (echo "msg $to $msg"; echo "safe_quit")
